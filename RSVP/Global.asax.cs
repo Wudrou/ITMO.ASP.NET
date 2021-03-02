f92f93029ea427e8752a28364fed6e30bc93749e
Wudrou <73528461+Wudrou@users.noreply.github.com>
2021-03-02 13:24:30 +0300

    Добавьте файлы проекта.

---
+++ RSVP/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace RSVP
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+        protected void Application_OnEndRequest()
+        {
+            Response.Write("<hr />Эта страница была загружена " + DateTime.Now.ToString());
+        }
+    }
+}
