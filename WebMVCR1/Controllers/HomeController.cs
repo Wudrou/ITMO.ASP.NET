8231c016ad2c140f85bf86f0ed8685b6322abeb4
Wudrou <73528461+Wudrou@users.noreply.github.com>
2021-03-17 22:55:45 +0300

    Add MVC

---
+++ WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMVCR1.Models;
+
+namespace WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Доброе утро" : "Добрый день";
+            ViewData["Mes"] = "хорошего настроения";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult InputData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult InputData(Person p)
+        {
+            db.AddResponse(p);
+            return View("Hello", p);
+        }
+
+        private static PersonRepository db = new PersonRepository();
+
+        public ViewResult OutputData()
+        {
+            ViewBag.Pers = db.GetAllResponses;
+            ViewBag.Count = db.NumberOfPerson;
+            return View("ListPerson");
+        }
+        //public string Index(string hel)
+        //{
+        //    string Greeting = ModelClass.ModelHello() + ", " + hel;
+        //    return Greeting;
+        //}
+    }
+}
