afba3057756c237b6bfec6becfd5bcdced42f855
Wudrou <73528461+Wudrou@users.noreply.github.com>
2021-03-19 15:17:19 +0300

    Add L7

--- MvcCreditApp1/App_Start/BundleConfig.cs
+++ MvcCreditApp1/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace MvcCreditApp1
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/ajax").Include(
+                      "~/Scripts/jquery.unobtrusive-ajax.min.js"));
         }
     }
 }
