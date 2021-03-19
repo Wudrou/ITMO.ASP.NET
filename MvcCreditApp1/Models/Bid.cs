8498e8fa2c7946ad6ce1fa9af07968b86e23da30
Wudrou <73528461+Wudrou@users.noreply.github.com>
2021-03-19 13:58:31 +0300

    Add L6 MvcCreditApp1

---
+++ MvcCreditApp1/Models/Bid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp1.Models
+{
+    public class Bid
+    {
+        // ID заявки
+        public virtual int BidId { get; set; }
+
+        // Имя заявителя
+        [DisplayName("Имя заявителя")]
+        [Required]
+        public virtual string Name { get; set; }
+
+        // Название кредита
+        [DisplayName("Название кредита")]
+        [Required]
+        public virtual string CreditHead { get; set; }
+
+        // Дата подачи заявки
+        [DisplayName("Дата подачи заявки")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
+        [Required]
+        public virtual DateTime bidDate { get; set; }
+    }
+}
---
+++ MvcCreditApp1/Models/Credit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp1.Models
+{
+    public class Credit
+    {
+        // ID кредита
+        public virtual int CreditId { get; set; }
+
+        // Название
+        [DisplayName("Наименование кредита")]
+        [Required]
+        public virtual string Head { get; set; }
+
+        // Период, на который выдается кредит
+        [DisplayName("Период кредита")]
+        [Required]
+        public virtual int Period { get; set; }
+
+        // Максимальная сумма кредита
+        [DisplayName("Максимальная сумма кредита")]
+        [Required]
+        public virtual int Sum { get; set; }
+
+        // Процентная ставка
+        [DisplayName("Процентная ставка")]
+        [Required]
+        public virtual int Procent { get; set; }
+    }
+}
